e4562337bbb545ec7e32fe9bd1526e1bbb62b43a
notsocoolshresth <shresth_2301ai22@iitp.ac.in>
2025-09-06 21:35:33 +0530

    backend update plz work

--- backend/flask_server.py
+++ backend/flask_server.py
@@ -92,5 +92,6 @@ def health_check():
     return jsonify({"status": "ok"})
 
 if __name__ == '__main__':
+    print("OK")
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
